27cb62da582e0719d62204e4dacfba98e1e8e164
SmuuSka <samuaikio@gmail.com>
2024-02-28 20:40:32 +0200

    added flag boolean for png output

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,51 @@
 use std::env;
 use sha2::{Digest, Sha256};
+use qr_code::QrCode;
+use qrcode_generator::QrCodeEcc;
 
-fn gen_qr(hash: &String) {
-    let qr_code = qr_code::QrCode::new(hash).unwrap();
-    print!("{}", qr_code.to_string(false,3));
+fn gen_qr(hash: &String, flag: &bool) {
+    let _path = format!("./target/release/{}.png", hash);
+    if !flag {
+        let qr_code = QrCode::new(hash).unwrap();
+        print!("{}", qr_code.to_string(false,3));
+        return;
+    }
+    qrcode_generator::to_png_to_file(&hash, QrCodeEcc::Low, 1024, _path).unwrap();
+    return;
 }
 
 fn hash(word: &String) -> String {
+    let mut _word = String::new();
+    let mut flag = false;
+    if word.len() < 5 {
+        let mut hasher = Sha256::new();
+        hasher.update(word);
+        let result = hasher.finalize();
+        let res = result[..].iter().map(|b| format!("{:02x}", b)).collect();
+        gen_qr(&res, &flag);
+        return res;
+    }
+    let sliced = &word[word.len() - 5..];
+    if sliced == "--png" {
+        _word = word[..word.len() - 6].to_string();
+        flag = true;
+    }
+    else {
+        _word = word.to_string();
+    }
     let mut hasher = Sha256::new();
-    hasher.update(word);
+    hasher.update(&_word);
     let result = hasher.finalize();
     let res = result[..].iter().map(|b| format!("{:02x}", b)).collect();
-    gen_qr(&res);
+    gen_qr(&res, &flag);
     return res;
 }
 
 fn read_args(args: Vec<String>) -> String {
     let mut word = String::new();
-
-    if args.len() < 2 {
+    return if args.len() < 2 {
         println!("No arguments given");
-        return String::from("");
+        String::from("")
     } else {
         for index in 1..args.len() {
             if index == args.len() - 1 {
@@ -30,7 +55,7 @@ fn read_args(args: Vec<String>) -> String {
                 word.push_str(" ");
             }
         }
-        return word;
+        word
     }
 }
 
@@ -67,6 +92,12 @@ mod tests {
     fn test_hash_with_many_words() {
         let result = hash(&String::from("testi testi testi"));
         assert_eq!(result, "407e5bacb511ffaa0a05f7d01d8b033755db188e0a893169a1f51cc42ff8f491");
+    }
+
+    #[test]
+    fn test_hash_with_flag() {
+        let result = hash(&String::from("moi --png"));
+        assert_eq!(result, "a4f0e1d1b5eb23e3482a14b4a9d4e8106e83f7887471e4a73c2557ade280bfe5");
     }
 
     #[test]
